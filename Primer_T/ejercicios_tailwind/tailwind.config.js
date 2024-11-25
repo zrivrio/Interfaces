abbc8ed938c9ffedf75ba24689b5ba77e543117d
zrivrio <zrivrio2103@gmail.com>
2024-11-25 10:34:26 +0100

    Bootstrapy tailwind

---
+++ Primer_T/ejercicios_tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./webs/**/*.{html,js}"],  // Busca en todos los archivos HTML y JS dentro de la carpeta webs
+  theme: {
+    extend: {},  // Aquí puedes agregar configuraciones personalizadas si las necesitas
+  },
+  plugins: [],
+}
