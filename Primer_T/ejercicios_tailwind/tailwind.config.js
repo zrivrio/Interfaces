560f50b121270c40c26fb7d34347232c0017ce09
zrivrio <zrivrio2103@gmail.com>
2024-11-26 08:23:14 +0100

    Bootstrap2

--- Primer_T/ejercicios_tailwind/tailwind.config.js
+++ Primer_T/ejercicios_tailwind/tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./webs/**/*.{html,js}"],  // Busca en todos los archivos HTML y JS dentro de la carpeta webs
-  theme: {
-    extend: {},  // Aquí puedes agregar configuraciones personalizadas si las necesitas
-  },
-  plugins: [],
+
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {  
+  content: ["./webs/**/*.{html,js}"],  /* webs es una carpeta en el nivel de tailwind.config.js */  
+  theme: {    
+    extend: {    
+      colors:{
+      'azul-claro':'#243cff',
+      'naranja':'#fdac72'
+    },
+    spacing:{
+      '41':'800px',
+    },
+    width:{
+      '39':'430px',
+    },
+    height:{
+      '39':'333px',
+    }
+  },  
+  },  
+  plugins: [], 
 }
