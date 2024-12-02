90b15c8cb8283d5f6366dbb11fdd36ca563ccc87
zrivrio <zrivrio2103@gmail.com>
2024-12-02 11:11:12 +0100

    Ejercicios tailwind

---
+++ Primer_T/Tailwind/ejercicio_1/tailwind.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {  
+  content: ["./webs/**/*.{html,js}"],  /* webs es una carpeta en el nivel de tailwind.config.js */  
+  theme: {    
+    extend: {},  
+  },  
+  plugins: [], 
+}
