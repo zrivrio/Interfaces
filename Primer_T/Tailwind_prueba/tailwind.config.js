82873b4ea152a4b3dfeb81dc393883c75ec9980c
zrivrio <zrivrio2103@gmail.com>
2024-12-10 11:12:57 +0100

    tailwind ejercicio 2

--- Primer_T/Tailwind_prueba/tailwind.config.js
+++ Primer_T/Tailwind_prueba/tailwind.config.js
@@ -4,27 +4,7 @@ module.exports = {
   content: ["./webs/**/*.{html,js}"],  /* webs es una carpeta en el nivel de tailwind.config.js */  
   theme: {    
     extend: {    
-      colors:{
-      'azul-claro':'#243cff',
-      'naranja':'#fdac72'
-    },
-    spacing:{
-      '41':'800px',
-    },
-    width:{
-      '39':'430px',
-    },
-    height:{
-      '39':'333px',
-    },
-    screens:{
-      'tablet':'900px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }
+
   },  
   },  
   plugins: [], 
