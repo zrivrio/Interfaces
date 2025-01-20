0b7574b64d309af8278a0c58fbaaed665ab503e4
Zahira <zrivrio2103@gmail.com>
2025-01-20 19:53:29 +0100

    repaso tailwind

--- Primer_T/Tailwind_prueba/tailwind.config.js
+++ Primer_T/Tailwind_prueba/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   content: ["./webs/**/*.{html,js}"],  /* webs es una carpeta en el nivel de tailwind.config.js */  
   theme: {    
     extend: {    
-
+      colors:{
+        primary:'#1E40AF',
+        secundary:'#F59E0B',
+        accent:'#F87171',
+        light:'#F3F4F6',
+      },
+      spacing:{
+        72:'18rem',
+        84:'21rem',
+        96:'24rem',
+      },
+      screens:{
+        tablet:'900px',
+      },
   },  
   },  
   plugins: [], 
